99caca15e08cddbc56d3269a362af53a50d62bc6
NoxSol9510 <sunz.woot@gmail.com>
2024-11-17 00:05:40 +0700

    Add constants.py and create a screen display

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
