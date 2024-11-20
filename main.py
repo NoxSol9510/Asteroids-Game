6c8d4f520d4de5aae7333709b86c318768ba123b
NoxSol9510 <sunz.woot@gmail.com>
2024-11-20 16:34:19 +0700

    Change the way the frame update to not create much lag

--- main.py
+++ main.py
@@ -7,27 +7,33 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    running = True
+    pygame.display.set_caption("Asteroids Game")
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    pygame.display.set_caption("Asteroids Game")
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while running:
+    while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # Close the window
-                running = False
+            if event.type == pygame.QUIT:
+                return
 
-        player.update(dt)
-        screen.fill((0, 0, 0))  # Clear the screen
-        player.draw(screen)     # Draw the player
-        pygame.display.flip()   # Update the display
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-    pygame.quit()  # Clean up and close the game
 
 
 if __name__ == "__main__":
